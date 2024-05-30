8c643ae8a80f8c2acab82569f4cd694963c05361
afeigin <afeigin@nvidia.com>
2024-05-30 18:43:31 +0300

    Make interface name length validation more generic

--- common/interface.h
+++ common/interface.h
@@ -8,13 +8,26 @@ namespace swss {
 
 inline bool isInterfaceNameLenOk(const std::string &ifaceName)
 {
-    if (ifaceName.length() >= IFNAMSIZ)
+    if (!validate_interface_name_length(ifaceName))
     {
         SWSS_LOG_ERROR("Invalid interface name %s length, it must not exceed %d characters", ifaceName.c_str(), IFNAMSIZ);
         return false;
     }
     return true;
 }
+
+#if defined(SWIG) && defined(SWIGPYTHON)
+%pythoncode %{
+    iface_name_max_length = IFNAMSIZ
+    def validate_interface_name_length(iface_name):
+    """
+    Verify that interface name length does not exceed IFNAMSIZ
+    """
+    if len(iface_name) == 0:
+        return False
+    return True if len(iface_name) < IFNAMSIZ else False
+%}
+#endif
 
 }
 
